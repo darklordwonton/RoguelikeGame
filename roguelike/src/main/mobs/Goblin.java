0bc1cac93fd6bc3936b4910ce2a11462abf91475
xtaticorange <xtaticorange@gmail.com>
2017-06-12 17:32:58 -0400

    basics for goblin

--- roguelike/src/main/mobs/Goblin.java
+++ roguelike/src/main/mobs/Goblin.java
@@ -5,16 +5,22 @@ import main.util.Globals;
 import main.entities.ai.*;
 
 public class Goblin {
+	int statDeviation = (int) Math.round(Math.random() * 2 * -1);
 	
-	private Stats stats = null;
 	
-	int low = 1;
-	int mid = 2;
-	int high = 3;
+	
+	
 	
 	Goblin(){
-//		change when ai complete
-		AI ai = new AI();
+		Stats stats = new Stats(Globals.midStat + statDeviation, 
+								Globals.lowStat + statDeviation, 
+								Globals.midStat + statDeviation, 
+								Globals.midStat + statDeviation,
+								Globals.midStat + statDeviation,
+								Globals.midStat + statDeviation,
+								Globals.currentFloor);
+								
+		
 	}
 	
 	public void move(){
