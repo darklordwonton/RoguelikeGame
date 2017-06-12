3e9decddbec301b476893c77c7c31e0e81f5f6e4
xtaticorange <xtaticorange@gmail.com>
2017-06-12 19:01:06 -0400

    a

--- roguelike/src/main/monsters/Goblin.java
+++ roguelike/src/main/monsters/Goblin.java
@@ -5,17 +5,17 @@ import main.entities.Stats;
 import main.util.Globals;
 
 public class Goblin extends Entity{
-	Stats ideal = new Stats (Globals.MID_STAT, 
-							Globals.MID_STAT, 
-							Globals.LOW_STAT, 
-							Globals.MID_STAT, 
-							Globals.MID_STAT, 
-							Globals.MID_STAT, 
+	Stats ideal = new Stats (Globals.MID_STAT, //hp
+							Globals.MID_STAT, //mp
+							Globals.LOW_STAT, //buf
+							Globals.MID_STAT, //tuf
+							Globals.MID_STAT, //wit
+							Globals.MID_STAT, //snk
 							Globals.floorNum);
 	
 	Goblin(String n, int px, int py){
 		super(n, px, py);
-			Stats stats = new Stats (Globals.floorNum*15, Globals.floorNum);
+			Stats stats = new Stats(15 + 5 * Globals.floorNum, Globals.floorNum);
 			stats.average(ideal);
 			
 	}
