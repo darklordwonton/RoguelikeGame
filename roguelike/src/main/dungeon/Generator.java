ebf3c5adc763182a29307eec91fdfca5b42083a3
xtaticorange <xtaticorange@gmail.com>
2017-06-13 12:35:31 -0400

    fix

--- roguelike/src/main/dungeon/Generator.java
+++ roguelike/src/main/dungeon/Generator.java
@@ -6,9 +6,9 @@ import main.util.Globals;
 
 public class Generator {
 	public static void newFloor (int spawnX, int spawnY, int stairsX, int stairsY, int creatureCount){
-		
 		EnumTheme[] cave = {EnumTheme.CAVE};
 		EnumTheme[] grayBricks = {EnumTheme.GRAY_BRICK_1, EnumTheme.GRAY_BRICK_2, EnumTheme.GRAY_BRICK_3, EnumTheme.GRAY_BRICK_4};
+		
 		Globals.currentFloor = new Floor(Globals.FLOOR_SIZE, Globals.FLOOR_SIZE, grayBricks, 15, 15);
 		Globals.currentFloor.addEntity(Globals.player, spawnX, spawnY);
 	}
