fc69abc9c80d88a26b0562281492c90afe84cb11
xtaticorange <xtaticorange@gmail.com>
2017-06-15 20:18:53 -0400

    wall color no longer shitty

--- roguelike/src/main/tiles/TileWall.java
+++ roguelike/src/main/tiles/TileWall.java
@@ -6,6 +6,6 @@ public class TileWall extends TileInteractive{
 	
 	public TileWall(EnumTheme t, int x, int y) {
 		super(x, y);
-		sprite = Globals.WALL_SPRITES.get(t);
+		sprite = Globals.EMPTY_SPRITES.get(t);
 	}
 }
