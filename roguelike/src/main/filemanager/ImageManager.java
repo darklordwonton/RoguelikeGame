4cb413c7d9cb9cfa8c4039cad3e2240dc25c65ee
darklordwonton <simonisgood66@gmail.com>
2017-06-28 23:33:38 -0400

    undid gitignore because I think it's breaking things

--- roguelike/src/main/filemanager/ImageManager.java
+++ roguelike/src/main/filemanager/ImageManager.java
@@ -47,7 +47,6 @@ public class ImageManager {
 	}
 	
 	public static BufferedImage getTileTexture (String image, int column, int row) {
-		System.out.println(image + " " + column + " " + row);
 		return tiles.get(image).getSubimage(column*16, row*16, 16, 16);
 	}
 	
