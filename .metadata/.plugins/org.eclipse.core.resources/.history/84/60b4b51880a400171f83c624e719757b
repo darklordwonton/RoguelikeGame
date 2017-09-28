927ced4c2cfef71e102f539124387e3752391661
TheGreatAtuin <sam@easterbrook.ca>
2017-09-28 15:17:49 -0400

    Names++

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/84/60b4b51880a400171f83c624e719757b
@@ -0,0 +1,12 @@
+package main.util;
+
+import main.rendering.MainRenderer;
+
+public class StringUtils {
+
+	public static String capitalize (String s){
+		return s.substring(0,1).toUpperCase() + s.substring(1);	
+	}
+
+
+}
--- roguelike/src/main/util/StringUtils.java
+++ roguelike/src/main/util/StringUtils.java
@@ -4,9 +4,17 @@ import main.rendering.MainRenderer;
 
 public class StringUtils {
 
-	public static String capitalize (String s){
+	public static String capitalize (String s) {
 		return s.substring(0,1).toUpperCase() + s.substring(1);	
 	}
 
-
+	public static String repeat (char c, int n) {
+		String s = "";
+		for (int i = 0; i < n; i++) {
+			s += c;
+		}
+		return s;
+	}
+	
+	
 }
