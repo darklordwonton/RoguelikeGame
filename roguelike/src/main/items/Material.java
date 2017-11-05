ecfdfa8a2cfb40f04b94c21d03364c05c688478e
TheGreatAtuin <sam@easterbrook.ca>
2017-11-05 16:04:12 -0500

    Created a class for materials

---
+++ roguelike/src/main/items/Material.java
@@ -0,0 +1,12 @@
+package main.items;
+
+public class Material {
+	String name = "";
+	int hardness = -1; //effectiveness as armor
+	int sharpness = -1; //effectiveness as weapon
+	
+	public Material() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
